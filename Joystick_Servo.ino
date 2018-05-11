d75da4966eedc6e2e9e535b5d7f1bb1117bcc2e6
AlvaroSC7 <34717370+AlvaroSC7@users.noreply.github.com>
2018-05-11 20:44:51 +0200

    Add files via upload

---
+++ Joystick_Servo.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos=255/2;    // variable to store the servo position
+int Joystick=A0;
+int JoystickValue;
+int pwmValue;
+int aux=0;
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+Serial.begin(9600);
+}
+
+void loop() {
+if(aux==0)
+{
+  myservo.write(pos);
+  delay(25);
+  aux++;
+}
+JoystickValue=analogRead(Joystick);
+pos=JoystickValue/4;
+myservo.write(pos);
+delay(25);
+Serial.println(pos);
+}
+
